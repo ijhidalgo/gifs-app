d821f5ee9cebdba8e158ea57f55b29db479d3994
Ivan <ijhidalgo@minsait.com>
2025-01-23 15:19:21 +0100

    Lazy images

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+
+import { CardComponent } from './components/card/card.component';
 import { CardlistComponent } from './components/card-list/card-list.component';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
@@ -13,9 +16,11 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     CardlistComponent,
     HomePageComponent,
     SearchBoxComponent,
+    CardComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent
